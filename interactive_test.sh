3259b20a3c59102b2c4c1097e6e2141585a83085
Markus Meissner <coder@safemailbox.de>
2020-08-25 23:44:58 +0200

    update interactive_test.sh script

--- interactive_test.sh
+++ interactive_test.sh
@@ -4,10 +4,19 @@ npy=venv/bin/nitropy
 
 function make_title
 {
-	echo "########################################################################"
-	echo "## $1"
-	echo "## $2"
-	echo -n ">> press enter to continue... "; read foo
+	if [[ "$2" = "" ]]; then 
+		echo "------------------------------------------------------------------------"
+		echo "-> $1"
+	else
+		echo "########################################################################"
+		echo "########################################################################"
+    echo "## $1"
+	fi
+
+	if [[ "$2" != "" ]]; then
+		echo "## $2"
+    echo -n ">> press enter to continue... "; read foo
+	fi
 }
 
 function askout
@@ -30,53 +39,110 @@ function run
 
 function testfido2
 {
-	make_title 'Testing Nitrokey - FIDO2' \
-		'Please insert a Nitrokey FIDO2 (will be wiped!)'
+	make_title "Testing Nitrokey - FIDO2" \
+		"Please insert a Nitrokey FIDO2 (will be wiped!)"
 
+	make_title "Simple listing of device(s)"
 	run ls
 	run fido2 list
-	run fido2 make-credential
 
+
+	make_title "create a credential + challenge-response using it"
+	
+	run fido2 make-credential
 	echo "press again..."
 	out=`${npy} fido2 make-credential | tail -n 1`
 	echo key $out
-
 	run fido2 challenge-response $out my_challenge
 
-	run fido2 reboot
 
+	make_title "reboot, version, verify, update, verify, reset, version"
+
+	run fido2 reboot
 	echo "sleeping for 10secs..."
 	sleep 10
 
+	run fido2 version
 	run fido2 verify
 	run fido2 update
 	run fido2 verify
 	run fido2 reset
+	run fido2 version
+
+
+	make_title "rnd subcommand(s)"
+
+	run fido2 rng hexbytes
+	run fido2 rng hexbytes --count 12
+
+	echo "SKIP: sudo run fido2 rng feedkernel"
+	echo "SKIP: run fido2 rng raw"
+
+	make_title "wink, reboot, wink, reboot, reset, set-pin, change-pin, verify"
+
+	run fido2 wink
+	run fido2 reboot
+	sleep 5
+	run fido2 wink
+	run fido2 reboot
+	sleep 5
+
+	# hrm ...
+	#echo -ne "1234\n1234\n" > set_pin.txt
+	#echo -ne "1234\n123456\n123456\n" > change_pin.txt
+
+	run fido2 reset
+	run fido2 set-pin
+	echo "make sure pin is finally: 123456"
+	run fido2 change-pin
+	run fido2 verify --pin 123456
+	
+	make_title "finally one more reset and then verify"
+	run fido2 reset
+	run fido2 verify
 
 }
 
 function teststart
 {
-	make_title 'Testing Nitrokey - Start' \
-		'Please insert a Nitrokey Start (will be wiped!)'
+	make_title "Testing Nitrokey - Start" \
+		"Please insert a Nitrokey Start (will be wiped!)"
 
+
+	make_title "Simple listing of devices"
+	
 	run ls
 	run start list
-	run start set-identity 0
-	run start set-identity 1
-	run start set-identity 2
-	run start set-identity 0
-	run start update
-	run start set-identity 0
-	run start set-identity 1
-	run start set-identity 2
-	run start set-identity 0
+	
+	make_title "setting identity 0, 1, 2, 0"
 
+	run start set-identity 0
+	run start set-identity 1
+	run start set-identity 2
+	run start set-identity 0
+	
+
+	make_title "updating with latest firmware"
+
+	run start update
+
+
+	make_title "setting identity 2, 0, 1, 0"
+
+	run start set-identity 2
+	run start set-identity 0
+	run start set-identity 1
+	run start set-identity 0
 }
 
-testfido2
+if [[ "$1" = "" ]] || [[ "$1" = "fido2" ]]; then
+	testfido2
+fi
 
-teststart
+if [[ "$1" = "" ]] || [[ "$1" = "start" ]]; then
+	teststart
+fi
+
 
 
 
