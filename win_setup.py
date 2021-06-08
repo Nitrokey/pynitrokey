d52476f39e0c2335f5c0e543bf6a2e82cc7ca7bc
Markus Meissner <coder@safemailbox.de>
2021-06-08 11:30:59 +0200

    remove hardcoded version in win_setup.py

--- win_setup.py
+++ win_setup.py
@@ -1,7 +1,6 @@
+import pathlib
 from cx_Freeze import setup, Executable
 
-# Dependencies are automatically detected, but it might need
-# fine tuning.
 buildOptions = dict(packages = [], excludes = [])
 
 msiOptions = dict(
@@ -15,8 +14,11 @@ executables = [
     Executable('nitropy.py', base=base)
 ]
 
+__version_path__ = pathlib.Path(__file__).parent.resolve().absolute() / "pynitrokey" / "VERSION"
+__version__ = open(__version_path__).read().strip()
+
 setup(name='pynitrokey',
-      version = '0.4.2',
+      version = __version__,
       description = 'Nitrokey Python Tools',
       options = dict(build_exe = buildOptions,
                      bdist_msi = msiOptions),
