a033c4dfb199026802e00e6972659288a4f24a83
Sosthène Guédon <sosthene@nitrokey.com>
2024-03-21 16:41:55 +0100

    Remove unused spsdk code

--- pynitrokey/trussed/bootloader/lpc55_upload/mboot/__init__.py
+++ pynitrokey/trussed/bootloader/lpc55_upload/mboot/__init__.py
@@ -7,22 +7,4 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 """Module implementing communication with the MCU Bootloader."""
-
-from typing import Union
-
-from .interfaces.buspal import MbootBuspalI2CInterface, MbootBuspalSPIInterface
-from .interfaces.sdio import MbootSdioInterface
-from .interfaces.uart import MbootUARTInterface
-from .interfaces.usb import MbootUSBInterface
-from .interfaces.usbsio import MbootUsbSioI2CInterface, MbootUsbSioSPIInterface
-from .mcuboot import McuBoot
-
-MbootDeviceTypes = Union[
-    MbootBuspalI2CInterface,
-    MbootBuspalSPIInterface,
-    MbootSdioInterface,
-    MbootUARTInterface,
-    MbootUSBInterface,
-    MbootUsbSioI2CInterface,
-    MbootUsbSioSPIInterface,
-]
+#
